b459b6c6a1f930587efd8770c2ed6e7e7c46b719
Fuad <fuad.mohamed@aait.edu.et>
2024-01-03 10:32:39 +0300

    text detector works , logger works, userinterface manger injector works

--- src/background/backgroundScript.js
+++ src/background/backgroundScript.js
@@ -1,31 +1,12 @@
-import TextAreaDetector from "../components/TextAreaDetector/textAreaDetector";
-import EventDispatcher from "../events/EventDispatcher/EventDispatcher";
-import Logger from "../components/Logger/logger";
-
-const logger = Logger;
-const eventDispatcher = EventDispatcher;
-const textAreaDetector = new TextAreaDetector();
-
-console.log('Background script loaded');
-// Listen for tab updates
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status === 'complete') {
-        logger.info(`Tab updated: ${tabId}`);
-        // Perform text area scanning when a tab is updated
-        scanTextAreasInTab(tab);
+var helow = "name"
+// Listen for messages from the content script
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.command === "sendLogger") {
+        // Process the Logger instance
+        var  loggerMessages = request.logger;
+        helow = loggerMessages;
+        console.log(loggerMessages);
+        sendResponse({result: "Logger received"});
     }
 });
-
-// Function to scan text areas in a tab
-function scanTextAreasInTab(tab) {
-    try {
-        logger.info(`Scanning text areas in tab: ${tab.id}`);
-        textAreaDetector.scanTextAreas();
-    } catch (error) {
-        logger.error(`Error during text area scanning: ${error.message}`);
-    }
-}
-
-
-// ...
-
+console.log("background script loaded");
